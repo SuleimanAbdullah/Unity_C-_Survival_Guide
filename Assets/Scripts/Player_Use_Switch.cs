e15daa617c50adbf3f8f1e83d0b286454f095aa8
Suleiman Abdullah <suleimanabdullahdev.gmail.com>
2021-07-30 19:46:20 +0300

    Learn Switch

---
+++ Assets/Scripts/Player_Use_Switch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Use_Switch : MonoBehaviour
+{
+    public int WeaponID;
+
+    void Update()
+    {
+        switch (WeaponID)
+        {
+            case 1:
+                Debug.Log(" Gun selected");
+                break;
+            case 2:
+                Debug.Log("Knife Selected");
+                break;
+            case 3:
+                Debug.Log("Machine gun Selected");
+                break;
+        }
+
+    }
+
+}
